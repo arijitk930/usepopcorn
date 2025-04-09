ec39ed64b447f6ec3032544923844a4efa2917d9
arijitk930 <arijitk930@gmail.com>
2025-04-10 02:19:45 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -315,7 +315,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWacthed, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
 
         const data = await res.json();
